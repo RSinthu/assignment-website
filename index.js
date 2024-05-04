c7f2316ee692000453a036caf440d9533951ba2d
laavanjan <130158996+laavanjan@users.noreply.github.com>
2024-05-05 03:26:26 +0530

    deleted images and updated files

--- index.js
+++ index.js
@@ -36,4 +36,41 @@ function changeBlueImage()  {
     container.style.backgroundImage = "url('resources/card1.jpg')";
 }
 
-//fbox2
+//fbox8
+
+function changeRedImage8()  {
+    var container = document.getElementById('fbox8');
+
+    container.style.backgroundImage = "url('resources/card8-red.jpg')";
+}
+
+function changeGreenImage8()  {
+    var container = document.getElementById('fbox8');
+
+    container.style.backgroundImage = "url('resources/card8-green.jpg')";
+}
+
+function changeBlueImage8()  {
+    var container = document.getElementById('fbox8');
+
+    container.style.backgroundImage = "url('resources/card8.jpg')";
+}
+
+//fbox7
+function changeRedImage7()  {
+    var container = document.getElementById('fbox7');
+
+    container.style.backgroundImage = "url('resources/card7-red.jpg')";
+}
+
+function changeBrownImage7()  {
+    var container = document.getElementById('fbox7');
+
+    container.style.backgroundImage = "url('resources/card7-brown.jpg')";
+}
+
+function changeSilverImage7()  {
+    var container = document.getElementById('fbox7');
+
+    container.style.backgroundImage = "url('resources/card7.jpg')";
+}
